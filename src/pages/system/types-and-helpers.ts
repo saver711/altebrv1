771146390146b63a0c064e5f2e15721e48ccbd6c
Ahmed Hassan <ahwork711@gmail.com>
2023-05-02 18:17:34 +0200

    finish system components gothering

--- src/pages/system/types-and-helpers.ts
+++ src/pages/system/types-and-helpers.ts
@@ -1,11 +1,23 @@
 // TYPES
-export type Card_TP = {
+export type Card_TP<T> = {
     id: string
     title: string
+    name?: T
     addLabel?: string
     viewLabel?: string
-    addComponent: JSX.Element
+    addComponent?: JSX.Element
     viewHandler?: () => void
 }
+
+export type FormNames_TP = 'partners' | "add_supplier" | "add_account"
+
+export type GlobalFormNames_TP = 'countries' | 'cities' | "districts" | 'colors' | 'classifications' | 'nationalities' | "karats" | "categories" | "markets"
+
+export type StonesFormNames_TP = "stones" |
+    "shapes" |
+    "qualities" |
+    "purities" |
+    "natures" | 'colors'
 // HELPERS
+
 
